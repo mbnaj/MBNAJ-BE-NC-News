fd78117a071a65a3ce6ac7f1d5cd0fe583cb8c6a
mbnaj <mbnaj@hotmail.com>
2022-03-28 13:21:39 +0100

    GET /api/topics is ready

---
+++ __tests__/app.test.js
@@ -0,0 +1,41 @@
+const devData = require('../db/data/test-data/index');
+const seed = require('../db/seeds/seed');
+const db = require('../db/connection');
+
+const app = require('../app');
+const request = require('supertest');
+
+
+afterAll(() => {
+  if (db.end) db.end();
+});
+
+beforeEach(()=>seed(devData));
+
+//////////////////////////////////////////////////////////////
+//TESTING => Part 2: Build the api
+//////////////////////////////////////////////////////////////
+
+
+describe('GET /api/topics', () => {
+  test('status:200, responds with an array of topic objects', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(topics).toBeInstanceOf(Array);
+        expect(topics).toHaveLength(3);
+        topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+
+
+});
