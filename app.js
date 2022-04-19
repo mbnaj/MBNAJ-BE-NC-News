45e5bb3d3af52798cf782bb18c7edef1d9330181
mbnaj <mbnaj@hotmail.com>
2022-04-19 10:43:15 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const apiRouter = require("./routers/router.api");
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.use('/api',apiRouter);
 
